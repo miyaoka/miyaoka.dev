b7026816c3c47a0ab873947623ecb9754ccdc144
Masaya Kazama <miyan@t-p.jp>
2020-12-22 08:55:54 +0900

    set og:title

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -18,6 +18,7 @@ export default function Post({
     <Layout>
       <Head>
         <title>{postData.title}</title>
+        <meta name="og:title" content={postData.title} />
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
