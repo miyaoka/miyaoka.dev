39dfd89bd7afdd1f18d51e71ff807f9a482e2ce5
Masaya Kazama <miyan@t-p.jp>
2020-12-21 23:31:50 +0900

    add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import '../styles/global.css'
 import { AppProps } from 'next/app'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
+
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
