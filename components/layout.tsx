aacd2c46ae5fb1143bdeb5629a25ddd258bf9272
Masaya Kazama <miyan@t-p.jp>
2020-12-23 07:43:24 +0900

    add links

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,6 +6,20 @@ import Link from 'next/link'
 export const siteTitle = 'miyaoka.dev'
 const siteDesc = 'miyaokaの備忘録'
 
+const linkList = [
+  {
+    title: 'twitter',
+    img: '/images/twitter-outline.svg',
+    url: 'https://twitter.com/miyaoka',
+  },
+
+  {
+    title: 'GitHub',
+    img: '/images/github-outline.svg',
+    url: 'https://github.com/miyaoka/miyaoka.dev',
+  },
+]
+
 export default function Layout({
   children,
   home,
@@ -28,8 +42,22 @@ export default function Layout({
             <a className={utilStyles.colorInherit}>{siteTitle}</a>
           </Link>
         </h1>
+        <div className={styles.headLinks}>
+          {linkList.map((link) => {
+            return (
+              <a
+                href={link.url}
+                title={link.title}
+                target="_blank"
+                rel="noopener"
+              >
+                <img src={link.img} width="32" height="32" />
+              </a>
+            )
+          })}
+        </div>
       </header>
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
     </div>
   )
 }
