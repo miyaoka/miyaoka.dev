88d15ff381e42d69a80d467d2485dc1b42fefe42
Masaya Kazama <miyan@t-p.jp>
2020-12-31 00:41:07 +0900

    .md更新時にHMRさせる

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+let isUpdating = false
+const postLib = `${__dirname}/lib/posts.ts`
+
+const onChangePost = () => {
+  if (isUpdating) return
+  isUpdating = true
+  const content = fs.readFileSync(postLib, 'utf-8')
+
+  // libファイルにコードを書き加えて強制的にHMRを起こす
+  fs.writeFileSync(
+    postLib,
+    `${content}\nconsole.log('updatedAt: ${new Date().toISOString()}')`
+  )
+  // 1秒後に元に戻す
+  setTimeout(() => {
+    fs.writeFileSync(postLib, content)
+    isUpdating = false
+  }, 1000)
+}
+require('chokidar')
+  .watch('./posts', { ignoreInitial: true })
+  .on('add', onChangePost)
+  .on('change', onChangePost)
