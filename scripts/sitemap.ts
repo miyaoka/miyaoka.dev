95cb6e26a8820fc07c1181f35fa3978a307a9435
Masaya Kazama <miyan@t-p.jp>
2021-01-06 21:09:32 +0900

    check server dir

--- scripts/sitemap.ts
+++ scripts/sitemap.ts
@@ -8,9 +8,13 @@ const nextDir = path.join(__dirname, '../.next')
 
 const BUILD_ID = fs.readFileSync(path.join(nextDir, '/BUILD_ID')).toString()
 
+const serverDir = fs.existsSync(path.join(nextDir, '/serverless'))
+  ? '/serverless'
+  : '/server'
+
 sitemap({
   baseUrl: site.host,
-  pagesDirectory: path.join(nextDir, '/serverless/pages'),
+  pagesDirectory: path.join(nextDir, serverDir, '/pages'),
   targetDirectory: './public',
   ignoredExtensions: ['js', 'map'],
   extraPaths: ['/'],
