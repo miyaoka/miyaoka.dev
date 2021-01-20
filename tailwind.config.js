ee48843458ff49dc3c17516d4069567d030579fb
Masaya Kazama <miyan@t-p.jp>
2021-01-20 20:40:16 +0900

    update color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
@@ -18,6 +20,11 @@ module.exports = {
         'MS PGothic',
         'sans-serif',
       ],
+    },
+    extend: {
+      colors: {
+        blue: colors.lightBlue,
+      },
     },
   },
   variants: {},
